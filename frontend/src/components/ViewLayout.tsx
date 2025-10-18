d63dc576f159623ef3d156532e7fb2a5e0dddc32
Max Weinzierl <supermaxman1@gmail.com>
2025-10-18 10:10:49 -0500

    Added shutdown warning

--- frontend/src/components/ViewLayout.tsx
+++ frontend/src/components/ViewLayout.tsx
@@ -3,6 +3,11 @@ import {
   ContentLayout,
   BreadcrumbGroupProps,
   AppLayoutProps,
+  Flashbar,
+  Button,
+  SpaceBetween,
+  Box,
+  Header,
 } from '@cloudscape-design/components';
 import { useContext } from 'react';
 
@@ -51,6 +56,45 @@ export function ViewLayout(props: ViewLayoutProps) {
         toolsHide
         onNavigationChange={(event) => setNavigationOpen(event.detail.open)}
         maxContentWidth={props.maxContentWidth}
+        stickyNotifications
+        notifications={
+          <Flashbar
+            items={[
+              {
+                id: 'funding-lapse',
+                type: 'warning',
+                dismissible: false,
+                header: (
+                  "Government Shutdown"
+                ),
+                content: (
+                  <SpaceBetween 
+                    direction="vertical"
+                    size="xxs"
+                  >
+                    <Box variant="p" color="inherit">
+                      The United States government has shut down due to a funding lapse.
+                      As a consequence, ICE has stopped publishing data on detainees. 
+                      Until the government reopens and ICE resumes publishing data, statistics on this site will be outdated.
+                    </Box>
+                    <Box variant="p" color="inherit">
+                      ICE last published data on September 25th, 2025.
+                      As the month of September was not fully published by ICE, statistics on this site are current through the end of August 2025.
+                    </Box>
+                    <Button
+                      onClick={() => {
+                        window.open('https://www.ice.gov/funding-lapse', '_blank');
+                      }}
+                      
+                    >
+                      Learn more
+                    </Button>
+                  </SpaceBetween>
+                ),
+              },
+            ]}
+          />
+        }
       />
       <NewsBanner />
     </div>
